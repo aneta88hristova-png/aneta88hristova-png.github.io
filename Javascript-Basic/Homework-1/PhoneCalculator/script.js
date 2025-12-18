426ea95d7b5a77c2cb8c4156483634868f9db0de
aneta88hristova-png <aneta.88.hristova@gmail.com>
2025-12-18 17:01:03 +0100

    Add phone calculator functionality in script.js
    Implement phone calculator functionality to calculate subtotal, tax, and total price based on user input.

---
+++ Javascript-Basic/Homework-1/PhoneCalculator/script.js
@@ -0,0 +1,27 @@
+function calculate() {
+  // Get user input
+  const quantity = document.getElementById("quantity").value;
+
+  // Convert to number
+  const numberOfPhones = Number(quantity);
+
+  // Fixed prices with const
+  const PRaICE_PER_PHONE = 119.95;
+  const TAX_RATE = 0.05; // 5%
+
+  // Calculate 
+  const subtotal = PRICE_PER_PHONE * numberOfPhones;
+  const taxAmount = subtotal * TAX_RATE;
+  const totalPrice = subtotal + taxAmount;
+
+  // Show result
+  document.getElementById(
+    "result"
+  ).innerHTML = `<h3>For ${numberOfPhones} phones:</h3>
+ <p>Subtotal: $${subtotal.toFixed(2)}</p>
+ <p>Tax (5%): $${taxAmount.toFixed(2)}</p>
+ <h3>Total: $${totalPrice.toFixed(2)}</h3>`;
+}
+
+// Calculate on page load
+calculate();
