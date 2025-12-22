4cae9b72d1b3aa5a0f828ede469bfab0e9914b3d
aneta88hristova-png <aneta.88.hristova@gmail.com>
2025-12-22 16:00:53 +0100

    Fix formatting and comments in PhoneCalculator script

--- Javascript-Basic/Homework-1/PhoneCalculator/script.js
+++ Javascript-Basic/Homework-1/PhoneCalculator/script.js
@@ -4,13 +4,12 @@ function calculate() {
 
   // Convert to number
   const numberOfPhones = Number(quantity);
-
-  // Fixed prices with const - RÄTT stavning här!
-  const PRICE_PER_PHONE = 119.95; // Rätt stavning
+ 
+  const PRICE_PER_PHONE = 119.95;
   const TAX_RATE = 0.05; // 5%
 
   // Calculate
-  const subtotal = PRICE_PER_PHONE * numberOfPhones; // Använder rätt variabel
+  const subtotal = PRICE_PER_PHONE * numberOfPhones;
   const taxAmount = subtotal * TAX_RATE;
   const totalPrice = subtotal + taxAmount;
 
