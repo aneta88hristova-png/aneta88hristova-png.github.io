d643ed4ab781ee92c4b98b8a772581094141d0a9
Your Nameaneta88hristova-png <aneta.88.gmail.com>
2025-12-24 00:07:04 +0100

    uppdate

--- Javascript-Basic/Homework-2/EvenOrOdd/script.js
+++ Javascript-Basic/Homework-2/EvenOrOdd/script.js
@@ -1,10 +1,29 @@
 function checkNumber() {
-  let num = prompt("Enter a number:");
+  let input = prompt("Enter a number:");
 
-  if (num % 2 === 0) {
-    alert("The number is even");
+  if (input === null || input.trim() === "") {
+    alert("Please enter a number");
+    return;
+  }
+
+  let numberValue = Number(input);
+
+  if (isNaN(numberValue)) {
+    alert(
+      "That's not a valid number! Please enter a number like 1, 2, 3, etc."
+    );
+    return;
+  }
+
+  if (!Number.isInteger(numberValue)) {
+    alert("Decimal numbers are neither even nor odd. Please enter an integer.");
+    return;
+  }
+
+  if (numberValue % 2 === 0) {
+    alert(numberValue + " is even");
   } else {
-    alert("The number is odd");
+    alert(numberValue + " is odd");
   }
 }
 
